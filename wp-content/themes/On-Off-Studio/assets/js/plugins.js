2d4af2f8d82433f1f431315cc68f6aae3b17b60a
patlegris <lecointre.patrick@laposte.net>
2015-08-23 01:33:37 +0200

    Slider ok

--- wp-content/themes/On-Off-Studio/assets/js/plugins.js
+++ wp-content/themes/On-Off-Studio/assets/js/plugins.js
@@ -1,4 +1,4 @@
-// Avoid `console` errors in browsers that lack a console.
+// Avoid `console` errors in browsers that lack a console.------
 (function() {
     var method;
     var noop = function () {};
@@ -21,4 +21,78 @@
     }
 }());
 
-// Place any jQuery/helper plugins in here.
+//SLIDER --------------------------------------------------------
+$(document).ready(function () {
+    $(".slider").simpleSlider();
+});
+
+var mainslider;
+var sliding = false;
+
+$(document).ready(function () {
+    // Default options
+    var options = {
+        slides: '.slide', // The name of a slide in the slidesContainer
+        swipe: true,    // Add possibility to Swipe
+        magneticSwipe: true, // Add 'magnetic' swiping. When the user swipes over the screen the slides will attach to the mouse's position
+        transition: "slide", // Accepts "slide" and "fade" for a slide or fade transition
+        slideTracker: true, // Add a UL with list items to track the current slide
+        slideTrackerID: 'slideposition', // The name of the UL that tracks the slides
+        slideOnInterval: true, // Slide on interval
+        interval: 10000, // Interval to slide on if slideOnInterval is enabled
+        animateDuration: 1500, // Duration of an animation
+        animationEasing: 'easeInOut', // Accepts: linear ease in out in-out snap easeOutCubic easeInOutCubic easeInCirc easeOutCirc easeInOutCirc easeInExpo easeOutExpo easeInOutExpo easeInQuad easeOutQuad easeInOutQuad easeInQuart easeOutQuart easeInOutQuart easeInQuint easeOutQuint easeInOutQuint easeInSine easeOutSine easeInOutSine easeInBack easeOutBack easeInOutBack
+        pauseOnHover: false, // Pause when user hovers the slide container
+        useDefaultCSS: false, // Add default CSS for positioning the slides
+        neverEnding: true // Create a 'neverending/repeating' slider effect.
+    };
+
+    $(".slider").simpleSlider(options);
+    mainslider = $(".slider").data("simpleslider");
+    /* yes, that's all! */
+
+    $(".slider").on("beforeSliding", function(event){
+        var prevSlide = event.prevSlide;
+        var newSlide = event.newSlide;
+        $(".slider .slide[data-index='" + prevSlide + "'] .slidecontent").fadeOut();
+        $(".slider .slide[data-index='" + newSlide + "'] .slidecontent").hide();
+
+        sliding = true;
+    });
+
+    $(".slider").on("afterSliding", function(event){
+        var prevSlide = event.prevSlide;
+        var newSlide = event.newSlide;
+        $(".slider .slide[data-index='"+newSlide+"'] .slidecontent").fadeIn();
+        sliding = false;
+    });
+
+    /**
+     * Control the slider by scrolling
+     */
+    $(window).bind('mousewheel', function(event) {
+        if(!sliding){
+            if(event.originalEvent.deltaY > 0){
+                mainslider.nextSlide();
+            }
+            else{
+                mainslider.prevSlide();
+            }
+        }
+    });
+
+    $(".slide#first").backstretch("assets/images/bg1.jpg");
+    $(".slide#sec").backstretch("assets/images/bg2.jpg");
+    $(".slide#thirth").backstretch("assets/images/bg3.jpg");
+    $(".slide#fourth").backstretch("assets/images/bg4.jpg");
+    $(".slide#fifth").backstretch("assets/images/bg5.jpg");
+
+    $('.slide .backstretch img').on('dragstart', function(event) { event.preventDefault(); });
+
+    $(".slidecontent").each(function(){
+        $(this).css('margin-top', -$(this).height()/2);
+    });
+});
+
+
+//Backstretch --------------------------------------------------------
