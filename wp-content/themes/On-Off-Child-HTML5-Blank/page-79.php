da60fb6089bcdb421a3bcbc7671e8ad8d1614a31
Ysmaen <lecointre.patrick@laposte.net>
2015-09-03 14:50:27 +0100

    menu orange collapse et href ok

--- wp-content/themes/On-Off-Child-HTML5-Blank/page-79.php
+++ wp-content/themes/On-Off-Child-HTML5-Blank/page-79.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
-<div class="col-lg-7 col-md-7 col-sm-7">
+<div class="col-lg-7">
     <!--            <div class="container">-->
     <!--            <h1>--><?php //single_post_title(); ?><!--</h1>-->
     <?php get_template_part('loop-event'); ?>
 </div>
 
-<div class="col-lg-3 col-md-3 col-sm-3">
+<div class="col-lg-3">
     <?php get_sidebar(); ?>
     <p>Test 3 colonnes</p>
 </div>
