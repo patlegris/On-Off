c982c86912679f54edc2e84317b00c0434ec2d83
Ysmaen <lecointre.patrick@laposte.net>
2015-09-06 01:55:25 +0100

    En cours page contact, Problème icone validation newsletter, modal ok sauf qu'il ne passe pas en premier plan...

--- wp-content/themes/On-Off-Child-HTML5-Blank/page-25.php
+++ wp-content/themes/On-Off-Child-HTML5-Blank/page-25.php
@@ -3,8 +3,8 @@
 
 <!--BLOC 1-->
 <div class="wrapper">
-    <img src="<?php echo get_template_directory_uri(); ?>/img/Expositions_BG01.png"
-         alt="Expositions ON-OFF Studio" class="clearB">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/Expositions_BG01.png" alt="Expositions ON-OFF Studio"
+         class="clearB">
 
     <div class="titreAT1">
         <h1>EXPOSITIONS</h1>
@@ -18,34 +18,28 @@
             <h4 class="txtAT03"><span class="txtAT02G">03 OCT</span> - 12 DEC 2015</h4>
             <h4 class="titreAT04">/--- TECHNIQUES CRÉATIVES ---/</h4>
 
-            <p class="txtAT04">Nunc ut elit non lorem elementum molestie. Fusce ipsum elit, porttitor in
-                cursus
-                eu, suscipit ut lacus. In massa est, commodo a orci non, dapibus tempus </p>
+            <p class="txtAT04">Nunc ut elit non lorem elementum molestie. Fusce ipsum elit, porttitor in cursus eu,
+                suscipit ut lacus. In massa est, commodo a orci non, dapibus tempus </p>
             <h4 class="txtAT03"><span class="txtAT02G">12 JAN</span>- 12 FEV 2016</h4>
             <h4 class="titreAT04">/--- TECHNIQUES CRÉATIVES ---/</h4>
 
-            <p class="txtAT04">Nunc ut elit non lorem elementum molestie. Fusce ipsum elit, porttitor in
-                cursus
-                eu, suscipit ut lacus. In massa est, commodo a orci non, dapibus tempus </p>
+            <p class="txtAT04">Nunc ut elit non lorem elementum molestie. Fusce ipsum elit, porttitor in cursus eu,
+                suscipit ut lacus. In massa est, commodo a orci non, dapibus tempus </p>
             <h4 class="txtAT03"><span class="txtAT02G">13 FEV</span>- 26 MARS 2016</h4>
             <h4 class="titreAT04">/--- TECHNIQUES CRÉATIVES ---/</h4>
 
-            <p class="txtAT04">Nunc ut elit non lorem elementum molestie. Fusce ipsum elit, porttitor in
-                cursus
-                eu, suscipit ut lacus. In massa est, commodo a orci non, dapibus tempus </p>
+            <p class="txtAT04">Nunc ut elit non lorem elementum molestie. Fusce ipsum elit, porttitor in cursus eu,
+                suscipit ut lacus. In massa est, commodo a orci non, dapibus tempus </p>
 
         </div>
     </div>
 
     <div class="txtAT1">
-        <p>Cras vitae lorem luctus, aliquam ligula in, facilisis neque. Nullam neque nibh, eleifend sit amet
-            lectus vitae, varius scelerisque magna. In metus risus, consequat sagittis dapibus vitae,
-            interdum
-            non nisi. Phasellus non nulla eget nunc pharetra vestibulum nec non leo. Donec sapien justo,
-            varius
-            vitae rutrum sit amet, gravida sagittis lacus. Nunc ut elit non lorem elementum molestie. Fusce
-            ipsum elit, porttitor in cursus eu, suscipit ut lacus. In massa est, commodo a orci non, dapibus
-            tempus mi. </p>
+        <p>Cras vitae lorem luctus, aliquam ligula in, facilisis neque. Nullam neque nibh, eleifend sit amet lectus
+            vitae, varius scelerisque magna. In metus risus, consequat sagittis dapibus vitae, interdum non nisi.
+            Phasellus non nulla eget nunc pharetra vestibulum nec non leo. Donec sapien justo, varius vitae rutrum sit
+            amet, gravida sagittis lacus. Nunc ut elit non lorem elementum molestie. Fusce ipsum elit, porttitor in
+            cursus eu, suscipit ut lacus. In massa est, commodo a orci non, dapibus tempus mi. </p>
     </div>
 </div>
 
@@ -60,7 +54,7 @@
                <span class="arrow-btn post-tag-arrow">
                     <i class="arrow-btn-icon jaa-icon-arrow-right post-tag-arrow-icon"></i>
                     <span class="arrow-btn-circle post-tag-arrow-circle"></span>
-                    <span class="arrow-btn-text2 post-tag-arrow-text"><a href="contact.html">CONTACTEZ-NOUS</a></span>
+                    <span class="arrow-btn-text2 post-tag-arrow-text"><a href="?page_id=148&lang=fr">CONTACTEZ-NOUS</a></span>
                 </span>
     </div>
 
@@ -69,27 +63,46 @@
 
 <!--BLOC 3-->
 <div class="wrapper3">
-    <img src="<?php echo get_template_directory_uri(); ?>img/trame_bl1_bl2.png" alt="">
-    <h4 class="txtAT05"><span class="txtAT05A"><span class="littleTXT">du</span> 07 <span
-                class="littleTXT">au</span> 11</span><span class="txtAT05B">OCT 2016</span><span
-            class="txtAT05C">/--- VIA HORIZON ---/</span></h4>
+    <img src="<?php echo get_template_directory_uri(); ?>/img/trame_bl1_bl2.png" alt="">
+    <h4 class="txtAT05"><span class="txtAT05A"><span class="littleTXT">du</span> 07 <span class="littleTXT">au</span> 11</span><span
+            class="txtAT05B">OCT 2016</span><span class="txtAT05C">/--- VIA HORIZON ---/</span></h4>
 
     <div class="BG04">
-        <p>Cras vitae lorem luctus, aliquam ligula in, facilisis neque. Nullam neque nibh, eleifend sit amet
-            lectus vitae, varius scelerisque magna. In metus risus, consequat sagittis dapibus.</p>
+        <p>Cras vitae lorem luctus, aliquam ligula in, facilisis neque. Nullam neque nibh, eleifend sit amet lectus
+            vitae, varius scelerisque magna. In metus risus, consequat sagittis dapibus.</p>
+
+        <!--BOUTON DYNAMIQUE-->
+        <div class="post-tag3">
+                    <span class="arrow-btn post-tag-arrow">
+                    <i class="arrow-btn-icon jaa-icon-arrow-right post-tag-arrow-icon"></i>
+                    <span class="arrow-btn-circle post-tag-arrow-circle"></span>
+                    <span class="arrow-btn-text post-tag-arrow-text"><a href="expositions-type.html">EN SAVOIR
+                            +</a></span>
+                    </span>
+        </div>
     </div>
 </div>
 
 <!--BLOC 4-->
 <div class="wrapper4">
     <img src="<?php echo get_template_directory_uri(); ?>/img/trame_bl2_bl3.png" alt="">
-    <h4 class="txtAT05"><span class="txtAT05A"><span class="littleTXT">du</span> 13 <span
-                class="littleTXT">au</span> 29</span><span class="txtAT05B">OCT 2016</span><span
-            class="txtAT05C">/--- EXPO PICAFLOR ---/</span></h4>
+    <h4 class="txtAT05"><span class="txtAT05A"><span class="littleTXT">du</span> 13 <span class="littleTXT">au</span> 29</span><span
+            class="txtAT05B">OCT 2016</span><span class="txtAT05C">/--- EXPO PICAFLOR ---/</span></h4>
 
     <div class="BG04">
-        <p>Cras vitae lorem luctus, aliquam ligula in, facilisis neque. Nullam neque nibh, eleifend sit amet
-            lectus vitae, varius scelerisque magna. In metus risus, consequat sagittis dapibus.</p>
+        <p>Cras vitae lorem luctus, aliquam ligula in, facilisis neque. Nullam neque nibh, eleifend sit amet lectus
+            vitae, varius scelerisque magna. In metus risus, consequat sagittis dapibus.</p>
+
+        <!--BOUTON DYNAMIQUE-->
+        <div class="post-tag3">
+                    <span class="arrow-btn post-tag-arrow">
+                    <i class="arrow-btn-icon jaa-icon-arrow-right post-tag-arrow-icon"></i>
+                    <span class="arrow-btn-circle post-tag-arrow-circle"></span>
+                    <span class="arrow-btn-text post-tag-arrow-text"><a href="expositions-type.html">EN SAVOIR
+                            +</a></span>
+                    </span>
+        </div>
+
     </div>
     <p class="numFinP">< 1 - 2 - 3 - 4 ></p>
 </div>
