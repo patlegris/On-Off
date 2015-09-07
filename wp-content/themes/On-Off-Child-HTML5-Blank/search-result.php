0a9969083f790756a7276864d4326d3c7df6feba
Ysmaen <lecointre.patrick@laposte.net>
2015-09-07 23:04:56 +0100

    Bugs bandeau sous slider et link "sous-menus". search -> mise en forme

---
+++ wp-content/themes/On-Off-Child-HTML5-Blank/search-result.php
@@ -0,0 +1,71 @@
+<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+    <!-- article -->
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+        <!-- post thumbnail -->
+        <?php if (has_post_thumbnail()) : // Check if thumbnail exists ?>
+            <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+            </a>
+        <?php endif; ?>
+
+        <!--BLOC 4-->
+        <div class="wrapper3">
+            <!--            <div class="blocASS1">-->
+            <!--                <h4 class="txtAT05C2">/--- BLANC TITANE ---/</h4>-->
+
+            <h4 class="txtAT05C2">
+                /--- <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a> ---/
+            </h4>
+
+            <!--BOUTON DYNAMIQUE-->
+            <div class="post-tag">
+                    <span class="arrow-btn post-tag-arrow">
+                    <i class="arrow-btn-icon jaa-icon-arrow-right post-tag-arrow-icon"></i>
+                    <span class="arrow-btn-circle post-tag-arrow-circle"></span>
+                    <span class="arrow-btn-text post-tag-arrow-text"><a href="<?php the_permalink(); ?>"
+                                                                        title="<?php the_title(); ?>">EN SAVOIR
+                            +</a></span>
+                    </span>
+            </div>
+            <?php the_post_thumbnail( 'medium' ); // Declare pixel size you need inside the array ?><br><br>
+            <!-- /post title -->
+
+
+            <!-- post details -->
+            <!--        <span class="date">--><?php //the_time('F j, Y'); ?><!---->
+            <?php //the_time('g:i a'); ?><!--</span>-->
+            <!--        <span class="author">--><?php //_e('Published by', 'html5blank'); ?><!---->
+            <?php //the_author_posts_link(); ?><!--</span>-->
+<!--        <span-->
+<!--            class="comments">--><?php //if (comments_open(get_the_ID())) comments_popup_link(__('Leave your thoughts', 'html5blank'), __('1 Comment', 'html5blank')); ?><!--</span>-->
+
+<!--            <p class="txtPad01">Lieu d’expérimentation, la Galerie des Galeries continue ici à imaginer de nouvelles-->
+<!--                formes-->
+<!--                d’expositions et invite des contributeurs issus de différents champs créatifs à penser ensemble un-->
+<!--                projet-->
+<!--                qui interroge l’art, la mode et le design. Le visiteur se retrouve au sein d’une circulation-->
+<!--                particulière-->
+<!--                dont le point de départ est toujours l’oeuvre.-->
+<!--                <br></p>-->
+            <!-- post title -->
+            <!-- /post details -->
+
+            <p class="txtPad01"><?php html5wp_excerpt('html5wp_index');?></p>
+
+<!--            --><?php //edit_post_link(); ?>
+
+    </article>
+    <!-- /article -->
+
+<?php endwhile; ?>
+
+<?php else: ?>
+
+    <!-- article -->
+    <article>
+        <h2><?php _e("Désolé, il n'y a pas de contenu.", 'html5blank'); ?></h2>
+    </article>
+    <!-- /article -->
+
+<?php endif; ?>
--- wp-content/themes/On-Off-Child-HTML5-Blank/search.php
+++ wp-content/themes/On-Off-Child-HTML5-Blank/search.php
@@ -1,19 +1,29 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+<!--BLOC 1-->
+<div class="wrapper">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/recherche.jpg" alt="Expositions ON-OFF Studio"
+         class="clearB">
 
-			<h1><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
+    <div class="titreAT1">
+        <h1>Votre recherche</h1>
+    </div>
+    <!-- section -->
+    <section>
 
-			<?php get_template_part('loop'); ?>
+        <h4 class="txtAT05C2">/--- <?php echo sprintf(__('%s Search Results for ', 'html5blank'),
+                $wp_query->found_posts);
+            echo get_search_query(); ?> ---/</h4>
 
-			<?php get_template_part('pagination'); ?>
+        <?php get_template_part('search-result'); ?>
 
-		</section>
-		<!-- /section -->
-	</main>
+        <?php get_template_part('pagination'); ?>
 
-<?php get_sidebar(); ?>
+    </section>
+
+</div>
+
+
+<?php //get_sidebar(); ?>
 
 <?php get_footer(); ?>
