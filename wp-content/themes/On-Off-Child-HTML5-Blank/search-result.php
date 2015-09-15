cb2c2a2d6279f22635d0cd0fd7cfb8ae14214b79
Ysmaen <lecointre.patrick@laposte.net>
2015-09-15 17:46:23 +0100

    OK avant reprise avec Maria

--- wp-content/themes/On-Off-Child-HTML5-Blank/search-result.php
+++ wp-content/themes/On-Off-Child-HTML5-Blank/search-result.php
@@ -19,7 +19,17 @@
             <!-- post title -->
             <?php the_post_thumbnail('medium'); // Declare pixel size you need inside the array ?><br><br>
             <!-- /post details -->
-            <a class="txtPad01"><?php html5wp_excerpt('html5wp_index'); ?></a><br>
+            <a class="txtPad01"><?php html5wp_excerpt('html5wp_index'); ?></a>
+            <!--BOUTON DYNAMIQUE-->
+<!--            <div class="post-tag3">-->
+<!--                    <span class="arrow-btn post-tag-arrow">-->
+<!--                    <i class="arrow-btn-icon jaa-icon-arrow-right post-tag-arrow-icon"></i>-->
+<!--                    <span class="arrow-btn-circle post-tag-arrow-circle"></span>-->
+<!--                    <span class="arrow-btn-text post-tag-arrow-text"><a href="expositions-type.html">EN SAVOIR-->
+<!--                            +</a></span>-->
+<!--                    </span>-->
+<!--            </div>-->
+            <br>
 
             <p class="date">Article publié le <?php the_time('j F Y'); ?></p><br><br><br>
             <!--BOUTON DYNAMIQUE-->
